78497a9d7e26023d93dae81b33c9ba85e645579d
Renildo15 <renildorabi@ufrn.edu.br>
2025-06-01 15:38:43 -0300

    feat: hooks transactions

--- src/hooks/transactions.ts
+++ src/hooks/transactions.ts
@@ -1,6 +1,7 @@
 import { fetcherSimple } from "@/api/api";
 import { uri } from "@/api/uri";
-import { FinancialSummary, Transaction } from "@/types";
+import { CreateTransaction, FinancialSummary, Transaction } from "@/types";
+import axios from "axios";
 import useSWR from "swr";
 
 
@@ -46,7 +47,7 @@ export function useTransactions(token:string, params?: IParams) {
 
     const url = `${uri}/transactions/list/${query.toString() ? `?${query.toString()}` : ''}`;
 
-    const { data, error, isLoading, isValidating, mutate } = useSWR<ITransactionsResponse>([url], () => fetcherSimple(url, token))
+    const { data, error, isLoading, isValidating, mutate } = useSWR<FinancialSummary>([url], () => fetcherSimple(url, token))
 
     return {
         data,
@@ -55,4 +56,116 @@ export function useTransactions(token:string, params?: IParams) {
         isValidating,
         mutate
     }
+}
+
+export function useTransaction(token:string, transactionUUID:string) {
+  const url = `${uri}/transactions/${transactionUUID}/detail/`;
+  const { data, error, isLoading, isValidating, mutate } = useSWR<Transaction>([url], () => fetcherSimple(url, token))
+
+  return {
+      data,
+      error,
+      isLoading,
+      isValidating,
+      mutate
+  }
+}
+
+export async function createTransaction(token:string, data:CreateTransaction) {
+    const url = `${uri}/transactions/create/`;
+
+    interface IResponse {
+        message: string;
+    }
+
+     try {
+    const res = await axios.post<IResponse>(url, data, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return res.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.debug('Axios error message:', error.message);
+      if (error.response) {
+        console.debug('Response data:', error.response.data);
+        console.debug('Response status:', error.response.status);
+        console.debug('Response headers:', error.response.headers);
+      } else if (error.request) {
+        console.error('Request data:', error.request);
+      } else {
+        console.error('Error setting up request:', error.message);
+      }
+    } else {
+      console.error('Unexpected error:', error);
+    }
+    throw error;
+  }
+}
+
+export async function updateTransaction(token:string, transactionUUID:string, data:CreateTransaction) {
+    const url = `${uri}/transactions/${transactionUUID}/update/`;
+
+    interface IResponse {
+        message: string;
+    }
+
+     try {
+    const res = await axios.post<IResponse>(url, data, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return res.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.debug('Axios error message:', error.message);
+      if (error.response) {
+        console.debug('Response data:', error.response.data);
+        console.debug('Response status:', error.response.status);
+        console.debug('Response headers:', error.response.headers);
+      } else if (error.request) {
+        console.error('Request data:', error.request);
+      } else {
+        console.error('Error setting up request:', error.message);
+      }
+    } else {
+      console.error('Unexpected error:', error);
+    }
+    throw error;
+  }
+}
+
+export async function deleteTransaction(token:string, transactionUUID:string) {
+    const url = `${uri}/transactions/${transactionUUID}/delete/`;
+
+    interface IResponse {
+        message: string;
+    }
+
+     try {
+    const res = await axios.post<IResponse>(url, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return res.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.debug('Axios error message:', error.message);
+      if (error.response) {
+        console.debug('Response data:', error.response.data);
+        console.debug('Response status:', error.response.status);
+        console.debug('Response headers:', error.response.headers);
+      } else if (error.request) {
+        console.error('Request data:', error.request);
+      } else {
+        console.error('Error setting up request:', error.message);
+      }
+    } else {
+      console.error('Unexpected error:', error);
+    }
+    throw error;
+  }
 }
