44a5885650d6a625def5b7e8a89a325961ada0b6
Renildo15 <renildorabi@ufrn.edu.br>
2025-06-02 20:19:55 -0300

    feat:preparando função de submit

--- src/components/ModalAddTransaction/index.tsx
+++ src/components/ModalAddTransaction/index.tsx
@@ -3,7 +3,7 @@ import Input from "../Input";
 import Modal from "../Modal";
 import SelectForm from "../SelectForm";
 import { locationOptions, transactionTypes } from "../../data";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import ModalAddCategory from "../ModalAddcategory";
 
 interface IModalAddTransactionProps {
@@ -13,29 +13,63 @@ interface IModalAddTransactionProps {
 
 export default function ModalAddTransaction(props: IModalAddTransactionProps) {
     const [showAddCategoryModal, setShowAddCategoryModal] = useState(false);
+
+    const nameRef = useRef<HTMLInputElement>(null)
+    const valueRef = useRef<HTMLInputElement>(null)
+    const dateRef = useRef<HTMLInputElement>(null)
+
+    const categoryRef = useRef<HTMLSelectElement>(null)
+    const locationRef = useRef<HTMLSelectElement>(null)
+    const typeRef = useRef<HTMLSelectElement>(null)
+    
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+
+        const name = nameRef.current?.value
+        const value = valueRef.current?.value
+        const date = dateRef.current?.value
+        const category = categoryRef.current?.value
+        const location = locationRef.current?.value
+        const type = typeRef.current?.value
+
+        console.log(
+            {
+                name,
+                value,
+                date,
+                category,
+                location,
+                type
+            }
+        )
+    }
+
     return (
         <Modal setIsModalOpen={props.setIsModalOpen}>
             <div className="flex flex-col gap-4 w-full justify-start">
                 <h2 className="text-2xl font-semibold">Nova Transação</h2>
-                <form className="flex flex-col gap-2 w-full">
+                <form className="flex flex-col gap-2 w-full" onSubmit={handleSubmit}>
                     <Input
                         label="Nome"
                         type="text"
                         placeholder="Nome da transação"
+                        ref={nameRef}
                     />
 
                     <Input
                         label="Valor"
                         type="text"
                         placeholder="Valor"
+                        ref={valueRef}
                     />
-                    <SelectForm options={categories} label={"Categoria"} optionDefault={"Selecione uma categoria"}/>
-                    <SelectForm options={locationOptions} label={"Onde está o dinheiro?"} optionDefault={"Selecione local"}/>
-                    <SelectForm options={transactionTypes} label={"Tipo de transação"} optionDefault={"Selecione tipo"}/>
+                    <SelectForm options={categories} label={"Categoria"} optionDefault={"Selecione uma categoria"} ref={categoryRef}/>
+                    <SelectForm options={locationOptions} label={"Onde está o dinheiro?"} optionDefault={"Selecione local"} ref={locationRef}/>
+                    <SelectForm options={transactionTypes} label={"Tipo de transação"} optionDefault={"Selecione tipo"} ref={typeRef}/>
                     
                     <Input
                         label="Data"
                         type="date"
+                        ref={dateRef}
                     />
                     <div className="flex justify-between w-full">
                         <button className="btn btn-success w-full text-white">Salvar</button>
